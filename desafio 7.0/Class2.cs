c2f2cfb05fd8c36c7e69000541f017c3f155b4c2
CARLOSPALMA1 <161914918+CARLOSPALMA1@users.noreply.github.com>
2024-04-05 20:23:24 -0600

    Add files via upload

---
+++ desafio 7.0/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora Calc = new Calculadora("Casio", "Fx-82");
+            Calc_Cientifica calc_ = new Calc_Cientifica("Casio", "Fx-82");
+
+            Console.WriteLine("Datos de la Calculadora");
+            Console.WriteLine($"Marca: {Calc.Marca}");
+            Console.WriteLine($"Modelo: {Calc.Serie}");f
---
+++ desafio 7.0/Programa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Desafio_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!!!");
+            Class1 totito = new Class1();
+            totito.Dibujar_Tablero();
+
+        }
+    }
+}
