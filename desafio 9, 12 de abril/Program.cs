ad8d21aaf8e4b40d24fc6838c7d5f9dbc732cf56
CARLOSPALMA1 <161914918+CARLOSPALMA1@users.noreply.github.com>
2024-04-12 21:31:32 -0600

    Add files via upload

---
+++ desafio 9, 12 de abril/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace correo
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string Texto = "Hola Carlos, Este es mi nuevo correo: juan89577@gmail.com , Enviame los documentos faltantes";
+
+            string PatronIdentificacion = @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b";
+
+            Regex regex = new Regex(PatronIdentificacion);
+
+            MatchCollection coincidir = regex.Matches(Texto);
+
+            foreach (Match match in coincidir)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+}
